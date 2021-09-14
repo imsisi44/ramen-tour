25b418a79f66da36b0427c565fd2a010577e6483
imsisi44 <littleboss1308@icloud.com>
2021-09-14 18:56:10 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'ramen' && password == '1234'
+      username == ENV["USER"] && password == ENV["PASSWORD"]
     end
   end
 end
