e53742d746e0ffaf03687c6ed680e69366f4087b
imsisi44 <littleboss1308@icloud.com>
2021-09-15 10:16:38 +0900

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth, if: :production? 
+  protect_from_forgery with: :exception
 
   private
 
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["USER"] && password == ENV["PASSWORD"]
